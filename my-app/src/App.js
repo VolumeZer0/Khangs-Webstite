7e2d5f40229a0cd06f7e2a2197dba5a6738fc331
VolumeZer0 <khangtran530@gmail.com>
2023-08-03 23:42:11 -0700

    added gradiant animation, name w/ font, started on particles animation

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <body className="Background-header">
+      <div>
+        <h1 className="Gradient-text"> Khangarator Tran </h1>
+      </div>
+    </body>
+  );
+
+}
+
+export default App;
+
