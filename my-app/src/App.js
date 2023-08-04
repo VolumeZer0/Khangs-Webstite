741b70b584e83487625fcaa48a5a116fb0466053
VolumeZer0 <khangtran530@gmail.com>
2023-08-04 00:57:26 -0700

    added particles to background, changed font of name, made page longer

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-
+import ParticlesBackground from './components/ParticlesBackground';
 function App() {
   return (
+    
     <body className="Background-header">
+      <ParticlesBackground/>
       <div>
-        <h1 className="Gradient-text"> Khangarator Tran </h1>
+        <h1 className="KhangTranGradient-text"> Khang Tran </h1>
       </div>
     </body>
   );
