ae58afdc6be98345a5aaccbf85f7f7d2d1e0cdf7
VolumeZer0 <khangtran530@gmail.com>
2023-08-06 01:05:55 -0700

    finished bio paragraph

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Khangs-Webstite
 My awesome website for awesome stuff and awesome people
+
+Update 8/5/23: I am awesome
+Update 8/6/23: I am awesome
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 import img1 from './images/tony.png'
 import ParticlesBackground from './components/ParticlesBackground';
@@ -14,7 +15,7 @@ function App() {
       <div className="Left-block">
         <h1 className="Khang-text Gradient-effect"> Khang </h1>
         <h1 className="Tran-text Gradient-effect"> Tran </h1>
-        <p className="Bio-text"> I am currently a student studying under the Computer Engineering major at UC San Diego. The languages I have worked with include Python, Java, React, and React Native. <br/><br/>My hobbies include programming, buildings things, gaming, and also socializing! So feel free to reach out!</p>
+        <p className="Bio-text"> I am currently a student studying UC San Diego as a Computer Engineering Major. The languages I have worked with include Python, Java, React, and React Native. <br/><br/> I love to learn about new things especially when it comes to coding concepts and new ideas.  <br/><br/> Besides coding, my hobbies include gaming, exercising, building things, and socializing! So feel free to reach out and contact me! </p>
 
       </div>
 
