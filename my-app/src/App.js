5e29fd8eb23afed17b36f089563b98dfb500762e
VolumeZer0 <khangtran530@gmail.com>
2023-08-06 00:15:31 -0700

    added new color gradient, organized code.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -14,7 +14,8 @@ function App() {
       <div className="Left-block">
         <h1 className="Khang-text Gradient-effect"> Khang </h1>
         <h1 className="Tran-text Gradient-effect"> Tran </h1>
-        <p className="Bio-text">The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. [beep] A single lap should be completed each time you hear this sound. [ding] Remember to run in a straight line, and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready, start.</p>
+        <p className="Bio-text"> I am currently a student studying under the Computer Engineering major at UC San Diego. The languages I have worked with include Python, Java, React, and React Native. My hobbies include programming, buildings things, gaming, and also socializing! So feel free to reach out!</p>
+
       </div>
 
     </body>
