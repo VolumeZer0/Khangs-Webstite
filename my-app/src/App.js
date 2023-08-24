7fbe382a7feb9950dfd2112381c11b8bace0c44e
Khang <khangtran530@gmail.com>
2023-08-23 19:00:56 -0700

    added right block, changed postion, changed last name position

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -16,7 +16,9 @@ function App() {
         <h1 className="Khang-text Gradient-effect"> Khang </h1>
         <h1 className="Tran-text Gradient-effect"> Tran </h1>
         <p className="Bio-text"> I am currently a student studying UC San Diego as a Computer Engineering Major. The languages I have worked with include Python, Java, React, and React Native. <br/><br/> I love to learn about new things especially when it comes to coding concepts and new ideas.  <br/><br/> Besides coding, my hobbies include gaming, exercising, building things, and socializing! So feel free to reach out and contact me! </p>
-
+      </div>
+      <div className="Right-block">
+        <p className="Bio-text"> </p>
       </div>
 
     </body>
